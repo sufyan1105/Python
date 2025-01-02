a2e3cf5c1b05d326b3b774f97468d8ecbdf468e7
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-02 19:24:14 +0530

    2

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("Hello world!")
+# print("Hello world!")
+
+# age = input("Enter your age : ")
+# print("Your age is ",age)
+
+# num1 = int(input("Enter your First number : "))
+# num2 = int(input("Enter your Second number : "))
+
+# sum = num1 + num2
+
+# print("The sum of both the numbers is :",sum)
+
