46f771d596a48e65365432fbf864d7928702be62
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-02 20:06:30 +0530

    Conditions

--- Conditions.py
+++ Conditions.py
@@ -16,3 +16,14 @@
 # else:
 #     print("The light is broken.")
 
+#Student Grade
+# marks = int(input("Enter your marks : "))
+# if (marks >= 90) :
+#     print("Grade A")
+# elif (marks >= 80 and marks < 90) :
+#     print("Grade B")
+# elif (marks >= 70 and marks < 80) :
+#     print("Grade C")
+# else :
+#     print("Grade D")
+
