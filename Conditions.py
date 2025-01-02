a782bceab0594f16cd104f6d586d09ca5c270cac
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-02 19:46:57 +0530

    conditions

---
+++ Conditions.py
@@ -0,0 +1,18 @@
+# Odd even code
+# num = int(input("Enter your number : "))
+# if num % 2 == 0 :
+#     print("The number is even")
+# else :
+#     print("The number is odd")
+
+# Traffic signal 
+# light = input("Enter the color : ")
+# if light == "red" :
+#     print("Stop")
+# elif light == "green" :
+#     print("Go")
+# elif light == "yellow" :
+#     print("Go Slow")
+# else:
+#     print("The light is broken.")
+
--- Practice.py
+++ Practice.py
@@ -8,22 +8,5 @@
 # sum = num1 + num2
 # print("The sum of both the numbers is :",sum)
 
-# Odd even code
-# num = int(input("Enter your number : "))
-# if num % 2 == 0 :
-#     print("The number is even")
-# else :
-#     print("The number is odd")
 
-# Traffic signal 
-
-light = input("Enter the color : ")
-if light == "red" :
-    print("Stop")
-elif light == "green" :
-    print("Go")
-elif light == "yellow" :
-    print("Go Slow")
-else:
-    print("The light is broken.")
 
