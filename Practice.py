2c82d08a1f0c5ef538bd224fca3f0a7d8306e22c
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-02 19:28:44 +0530

    Practice

--- Practice.py
+++ Practice.py
@@ -1,15 +1,19 @@
 # print("Hello world!")
-
 # age = input("Enter your age : ")
 # print("Your age is ",age)
 
+#  Sum of 2 numbers
 # num1 = int(input("Enter your First number : "))
 # num2 = int(input("Enter your Second number : "))
-
 # sum = num1 + num2
-
 # print("The sum of both the numbers is :",sum)
 
+# Odd even code
+# num = int(input("Enter your number : "))
+# if num % 2 == 0 :
+#     print("The number is even")
+# else :
+#     print("The number is odd")
 
 
 
