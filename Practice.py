17c7b2919dee328b1c84d67c614909e7d248a737
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-03 19:01:13 +0530

    pratice

--- Practice.py
+++ Practice.py
@@ -8,5 +8,9 @@
 # sum = num1 + num2
 # print("The sum of both the numbers is :",sum)
 
+num1 = int(input("Enter number 1 : "))
+num2 = int(input("Enter number 2 : "))
+sum = num1 + num2
 
+print("The sum of 2 numbers is", sum)
 
