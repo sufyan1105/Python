31dd2a02f860f926582d2a6adc7e0ef87047e8c2
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-03 19:30:55 +0530

    conditions

--- Conditions.py
+++ Conditions.py
@@ -27,3 +27,14 @@
 # else :
 #     print("Grade D")
 
+# The greatest of 3 numbers 
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+c = int(input("Enter the third number : "))
+
+if (a >= b and a >= c) :
+    print("The greatest number is : ", a)
+elif (b >= c) :
+    print("The greatest number is : ", b)
+else :
+    print("The greatest number is : ", c)
