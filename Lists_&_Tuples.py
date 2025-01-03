2e8e1449f4696f68fba9c606d65e835cd0b4dc49
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-03 19:48:38 +0530

    lists

--- Lists_&_Tuples.py
+++ Lists_&_Tuples.py
@@ -1,4 +1,23 @@
-marks = [91,97,98,64,45]
-print(marks[0])
-print(marks[1])
-print(len(marks))
+#  Strings are Immutabel
+#  Lists are Mutable
+
+# marks = [91,97,98,64,45]
+# print(marks[0])
+# print(marks[1])
+# print(len(marks))
+
+# student = ["Sufyan" , 21 , "Mumbai"]
+# print("Your name is",student[0],". You are",student[1],"years old. You live in",student[2])
+# student[0] = "Sam"
+# print(student)
+
+# marks = [91,97,98,64,45]
+# print(marks[1:4])
+
+# List Methods
+list = [1,2,3,4,5,6,7,8,9]
+print(list.append(10))
+print(list.sort())
+print(list.sort(reverse=True))
+print(list.reverse)
+print(list.insert(0))
