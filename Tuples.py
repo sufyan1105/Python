691d45a37310a3cf892042ae4e60260d0f4f7c75
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-05 00:20:18 +0530

    Tuples

--- Tuples.py
+++ Tuples.py
@@ -1,4 +1,8 @@
 #  Tuples are Immutable just like strings. Lists are Mutable
 
-Tup = (12,13,14,15)
+Tup = (12,13,14,15,13)
 print(type(Tup))
+
+
+print(Tup.index(13)) # Returns the index of the first occurence of the element
+print(Tup.count(13)) # Returns the number of occurences of the element
