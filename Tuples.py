82aeeeb0330ed69b251dbfd85ba9a874d0fe9140
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-05 00:44:48 +0530

    Tuples

--- Tuples.py
+++ Tuples.py
@@ -1,8 +1,27 @@
 #  Tuples are Immutable just like strings. Lists are Mutable
 
-Tup = (12,13,14,15,13)
-print(type(Tup))
+# Tup = (12,13,14,15,13)
+# print(type(Tup))
 
 
-print(Tup.index(13)) # Returns the index of the first occurence of the element
-print(Tup.count(13)) # Returns the number of occurences of the element
+# print(Tup.index(13)) # Returns the index of the first occurence of the element
+# print(Tup.count(13)) # Returns the number of occurences of the element
+
+# movie1 = input("Enter the name of the first movie: ")
+# movie2 = input("Enter the name of the second movie: ")
+# movie3 = input("Enter the name of the third movie: ")
+
+# movies_list = []
+# movies_list.append(movie1)
+# movies_list.append(movie2)
+# movies_list.append(movie3)
+# print(movies_list)
+
+palindrome = input("Enter a word: ")
+list = list(palindrome)
+copy = list.copy()
+copy.reverse()
+if list == copy:
+    print("The word is a palindrome")
+else:
+    print("The word is not a palindrome")
