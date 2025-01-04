c76d0f0231012fbbc44bd6639b853622d6e96ad6
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-05 00:51:08 +0530

    Tuples

--- Tuples.py
+++ Tuples.py
@@ -2,26 +2,27 @@
 
 # Tup = (12,13,14,15,13)
 # print(type(Tup))
-
-
 # print(Tup.index(13)) # Returns the index of the first occurence of the element
 # print(Tup.count(13)) # Returns the number of occurences of the element
 
+# code to add the entered strings in a list
 # movie1 = input("Enter the name of the first movie: ")
 # movie2 = input("Enter the name of the second movie: ")
 # movie3 = input("Enter the name of the third movie: ")
-
 # movies_list = []
 # movies_list.append(movie1)
 # movies_list.append(movie2)
 # movies_list.append(movie3)
 # print(movies_list)
 
-palindrome = input("Enter a word: ")
-list = list(palindrome)
-copy = list.copy()
-copy.reverse()
-if list == copy:
-    print("The word is a palindrome")
-else:
-    print("The word is not a palindrome")
+# Code to check if the entered word is a palindrome or not
+# palindrome = input("Enter a word: ")
+# list = list(palindrome)
+# copy = list.copy()
+# copy.reverse()
+# if list == copy:
+#     print("The word is a palindrome")
+# else:
+#     print("The word is not a palindrome")
+
+
