90dbd75427eb24330bac82598927eab12b50de1e
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-06 20:13:44 +0530

    Sets

---
+++ sets.py
@@ -0,0 +1,12 @@
+collection = {1,2,3,4, "hello","world", 2,3}
+collection2 = {1,2,"world", 2,3}
+# print(type(collection))
+# print(len(collection))
+# print(collection.add(5))
+# print(collection.remove(5))
+# print(collection.pop())
+# print(collection.clear())
+# print(collection.union(collection2))
+# print(collection.intersection(collection2))
+
+print(collection)
