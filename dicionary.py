8e5701acfabac40c5a8646bbf2bcc9de5dc9f076
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-06 19:40:02 +0530

    dictionary

--- dicionary.py
+++ dicionary.py
@@ -2,6 +2,12 @@ student = {
     "name": "Mark",
     "student_id": 15163,
     "marks": [70, 80, 90, 60, 50],
+    "age": 25,
 }
 
 print(student["name"])
+
+student["name"] = "James"
+
+print(student["name"])
+
