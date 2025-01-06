4ef46eec67175840692a89e010db6ad198ddee1a
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-06 19:48:31 +0530

    dictionaries

--- dicionary.py
+++ dicionary.py
@@ -1,13 +1,31 @@
+# Dictionaries
+# student = {
+#     "name": "Mark",
+#     "student_id": 15163,
+#     "marks": [70, 80, 90, 60, 50],
+#     "age": 25,
+# }
+# print(student["name"])
+# student["name"] = "James"
+# print(student["name"])
+
+# Nested dictionaries
+
 student = {
-    "name": "Mark",
-    "student_id": 15163,
-    "marks": [70, 80, 90, 60, 50],
-    "age": 25,
+    "name" : "Mark",
+    "subjects" : {
+        "maths" : 70,
+        "physics" : 80,
+        "chemistry" : 90,
+    }
 }
+# print(student)
+# print(student["subjects"]["maths"])
+# print(student.keys())
+# print(list(student.keys()))
+# print(student.values())
+print(len(student))
 
-print(student["name"])
 
-student["name"] = "James"
 
-print(student["name"])
 
