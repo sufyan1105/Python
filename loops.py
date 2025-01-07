e12669414b063c4b966f6fabfbbb9fe128fd413c
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-07 18:49:37 +0530

    Loops

---
+++ loops.py
@@ -0,0 +1,14 @@
+# Loops
+# While loops 
+# For loops
+
+# count = 1 #Iterator
+# while count <= 5:
+#     print('Hello, world!')
+#     count += 1
+
+i = 1
+while i <= 10:
+    print("hello")
+    i += 1
+
