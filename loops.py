08a631fe8e622eb8322b50ce982dae7d1b93d785
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-07 18:53:35 +0530

    Practice question 1 & 2

--- loops.py
+++ loops.py
@@ -7,8 +7,21 @@
 #     print('Hello, world!')
 #     count += 1
 
-i = 1
-while i <= 10:
-    print("hello")
-    i += 1
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i += 1
 
+# Q1
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i += 1
+
+# Q2
+# i = 100
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+# Q3
