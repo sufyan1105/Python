e8833630349bd00e542b1e31ac234d506f0a810b
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-11 20:57:41 +0530

    Functions

--- functions.py
+++ functions.py
@@ -7,9 +7,16 @@
 # print(average(1,2,3))
 
 # Q1: Write a function that takes a string as an argument and returns the length of the string
-def length(a):
-    return len(a)
-sen = "Hello, my name is John"
-print(length(sen))
+# def length(a):
+#     return len(a)
+# sen = "Hello, my name is John"
+# print(length(sen))
 
+# Q2: Write a function that takes a list as an argument and returns the sum of the list on the same line
+def print_list(list):
+    for i in list:
+        print(i, end=" ")
+    
+list = [1,2,3,4,5]
+print_list(list)
 
