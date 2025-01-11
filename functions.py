3d3c95ae6b370e58c27f438f133f2e448fdc021f
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-11 21:08:04 +0530

    Q5

--- functions.py
+++ functions.py
@@ -28,8 +28,16 @@
 # print(fact(5))
 
 # Q4: USD to INR conversion
-def conversion(a):
-    inr = a*83
-    return (f"The amount in INR is {inr}")
-print(conversion(73))
+# def conversion(a):
+#     inr = a*83
+#     return (f"The amount in INR is {inr}")
+# print(conversion(73))
 
+# Q5: A function that returns if the number is even or odd
+def even_odd(n):
+    if n % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+print(even_odd(4))
+print(even_odd(5))
