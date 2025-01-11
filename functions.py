4e265b6a59eca3bc36f454ca5ca662e1adf2482a
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-11 20:53:12 +0530

    Q1

--- functions.py
+++ functions.py
@@ -5,3 +5,11 @@
 # def average(a,b,c):
 #     return (a+b+c)/3
 # print(average(1,2,3))
+
+# Q1: Write a function that takes a string as an argument and returns the length of the string
+def length(a):
+    return len(a)
+sen = "Hello, my name is John"
+print(length(sen))
+
+
