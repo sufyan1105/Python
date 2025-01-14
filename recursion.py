911adb714e7bb07f4fa860b6a568720b4efd2c41
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-14 19:41:50 +0530

    Q1

--- recursion.py
+++ recursion.py
@@ -7,9 +7,17 @@
 # show(5)
 
 # Recursion returns n! (n factorial)
-def fact(n):
-    if n == 0:
-        return 1
+# def fact(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * fact(n-1)
+# print(fact(5))
+
+# Recursive function to calculate the sum of n natural numbers
+def sum(n):
+    if n == 1:
+        return 0
     else:
-        return n * fact(n-1)
-print(fact(5))
+        return n + sum(n-1)
+print(sum(5))
