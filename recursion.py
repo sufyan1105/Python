5c7dbdff33ac90f9ee5f55a7564c75b3921f7266
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-14 19:33:31 +0530

    Factorial using recursion

--- recursion.py
+++ recursion.py
@@ -1,7 +1,15 @@
-def show(n):
-    if n == 0:
-        return
-    print(n)
-    show(n-1)
+# Recursive function 
+# def show(n):
+#     if n == 0:
+#         return
+#     print(n)
+#     show(n-1)
+# show(5)
 
-show(5)
+# Recursion returns n! (n factorial)
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n-1)
+print(fact(5))
