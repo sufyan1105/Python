5ecd9d1ce6d52a78f92509b916b0f009db6b5e59
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-14 19:49:49 +0530

    Q2

--- recursion.py
+++ recursion.py
@@ -15,9 +15,19 @@
 # print(fact(5))
 
 # Recursive function to calculate the sum of n natural numbers
-def sum(n):
-    if n == 1:
-        return 0
-    else:
-        return n + sum(n-1)
-print(sum(5))
+# def sum(n):
+#     if n == 1:
+#         return 0
+#     else:
+#         return n + sum(n-1)
+# print(sum(5))
+
+def print_list(list, idx=0):
+    if idx == len(list):
+        return 
+    print(list[idx])
+    print_list(list, idx+1)
+
+# list = [1, 2, 3, 4, 5]
+list = ['a', 'b', 'c', 'd', 'e']
+print_list(list)
