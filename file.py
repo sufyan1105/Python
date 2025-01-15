27002f0c905a5c5f73603bf56cabc87212de6d63
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-15 11:57:25 +0530

    Os module

--- file.py
+++ file.py
@@ -1,10 +1,11 @@
+import os
+
 f = open('practice.txt', 'a')
 # data = f.read()
 # print(data)
 
 f.write("My name is sufyan ")
 f.write("I am a student")
-
-
-
 f.close()
+
+os.remove('practice.txt')
