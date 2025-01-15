0bb884680cf997c5e85153dc56540b36b33f1815
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-15 11:44:51 +0530

    File i/o

--- file.py
+++ file.py
@@ -1,4 +1,10 @@
-f = open('practice.txt', 'r')
-data = f.read()
-print(data)
+f = open('practice.txt', 'a')
+# data = f.read()
+# print(data)
+
+f.write("My name is sufyan ")
+f.write("I am a student")
+
+
+
 f.close()
