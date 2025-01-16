014f113be1e57ed7a8a68a1a8f771c9d91a788a3
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-16 15:48:15 +0530

    First commit

---
+++ OOPS_1.py
@@ -0,0 +1,9 @@
+#  Class is a blueprint for creating objects (a particular data structure), providing initial values for state (member variables or attributes), and implementations of behavior (member functions or methods).    
+
+class Student:
+    name = "Sufyan"
+    age = 21
+
+s1 = Student()
+print(s1.name)
+
