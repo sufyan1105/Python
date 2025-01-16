363fe14c289278b2f4deb80c41363a6f7d8cddd4
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-16 19:15:56 +0530

    private class

---
+++ OOPS_2.py
@@ -0,0 +1,12 @@
+"""
+Private Class:
+    - This class is intended for internal use within the module.
+    - It is not meant to be accessed directly by users of the module.
+    - Typically, its name starts with an underscore ( __ ) to indicate its private nature.
+
+Public Class:
+    - This class is intended for use by users of the module.
+    - It provides the main functionality that the module is designed to offer.
+    - It is accessible and documented for external use.
+"""
+
