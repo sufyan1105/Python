038e1ad111ecd0b79477fecf4a32a186be86e997
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-16 15:24:36 +0530

    Q3 complete & File i/o completed

--- file.py
+++ file.py
@@ -29,7 +29,13 @@ import os
 #     else:
 #         print("No")
 
+count = 0
 with open("practice.txt", "r") as f:
     data = f.read()
+    nums = data.split(",")
+    for val in nums:
+        if(int(val) % 2 == 0):
+            count += 1
+print(count)
 
 
