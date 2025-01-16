f1f6d35876feb5febb0da70964427b6a956d3630
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-16 16:33:05 +0530

    Q1

--- OOPS_1.py
+++ OOPS_1.py
@@ -1,13 +1,13 @@
 #  Class is a blueprint for creating objects (a particular data structure), providing initial values for state (member variables or attributes), and implementations of behavior (member functions or methods).    
 
-class Student:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        print("Constructor Called")
+# class Student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         print("Constructor Called")
 
-s1 = Student("Sufyan", 21)
-print(s1.name, s1.age)
+# s1 = Student("Sufyan", 21)
+# print(s1.name, s1.age)
 
 # print(s1.name)
 
@@ -16,4 +16,19 @@ print(s1.name, s1.age)
 # c1 = Car()
 # print(c1.color)  # Output: Blue
 
+# Q1
+class Student:
+    def __init__(self, name , marks):
+        self.name = name
+        self.marks = marks
 
+    def FindAverage(self):
+        sum = 0
+        for val in self.marks:
+            sum += val
+        print("Hello", self.name,"your average marks are", round(sum/len(self.marks),2))
+        
+
+s1 = Student("Sufyan", [90,92,96])
+s1.FindAverage()
+        
