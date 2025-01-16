532c5f07aa5d51e32d1ef447490cccb5e4553345
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-16 15:57:32 +0530

    OOP

--- OOPS_1.py
+++ OOPS_1.py
@@ -1,9 +1,16 @@
 #  Class is a blueprint for creating objects (a particular data structure), providing initial values for state (member variables or attributes), and implementations of behavior (member functions or methods).    
 
-class Student:
-    name = "Sufyan"
-    age = 21
+# class Student:
+#     name = "Sufyan"
+#     age = 21
 
-s1 = Student()
-print(s1.name)
+# s1 = Student()
+# print(s1.name)
+
+class Car:
+    color = "Blue"
+    
+c1 = Car()
+print(c1.color)  # Output: Blue
+
 
