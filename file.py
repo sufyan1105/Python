02a1191eeb65c885bcfa3bb6973cff9ba0735e1d
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-16 14:48:12 +0530

    Q2

--- file.py
+++ file.py
@@ -10,7 +10,21 @@ import os
 
 # os.remove('practice.txt')
 
-with open("practice.txt", "w") as f:
-    f.write("My name is sufyan ")
-    f.write("I am a student")
+# with open("practice.txt", "w") as f:
+#     f.write("My name is sufyan ")
+#     f.write("I am a student")
     
+# with open("practice.txt", "r") as f:
+#     data = f.read()
+#     print(data)
+# newData = data.replace("sufyan", "Joy")
+# print(newData)
+# with open("practice.txt", "w") as f:
+#     f.write(newData)
+
+with open("practice.txt", "r") as f:
+    data = f.read()
+    if "name" in data:
+        print("Yes")
+    else:
+        print("No")
