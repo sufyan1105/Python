65f128bd3996492e77c8f9e76434c0ec359642b8
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-17 21:12:13 +0530

    Optimised Random Password Generator

--- Password_Generator.py
+++ Password_Generator.py
@@ -4,9 +4,14 @@ import string
 
 charValues = string.ascii_letters + string.digits + string.punctuation
 
-print(random.choice(charValues))
-password = ""
+# print(random.choice(charValues))
+# password = ""
+# n = int(input("Enter the length of the password: "))
+# for i in range(n):
+#     password += random.choice(charValues)
+
+# Using list comprehension
 n = int(input("Enter the length of the password: "))
-for i in range(n):
-    password += random.choice(charValues)
+password = "".join(random.choice(charValues) for i in range(n))
+
 print(password)
