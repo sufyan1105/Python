40b7b1c266b79cfed9402343f1939de0e87dadaf
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-24 22:44:10 +0530

    User score

--- Blackjack/main.py
+++ Blackjack/main.py
@@ -7,6 +7,7 @@ def deal_card():
     return card
 
 def calculated_cards(cards):
+    #Take a list of cards and return the score calculated from the cards
     if sum(cards) == 21 and len(cards) == 2:
         return 0
     
@@ -18,10 +19,20 @@ def calculated_cards(cards):
 
 user_cards = []
 computer_cards = []
+is_game_over = False
 
 for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
+
+user_score = calculated_cards(user_cards)
+computer_score = calculated_cards(computer_cards)
+print(f"Your cards: {user_cards}, current score: {user_score}")
+print(f"Computer's first card: {computer_cards[0]}")
+
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
 
 
 
