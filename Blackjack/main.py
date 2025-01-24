c1174b28072a30a9bf18252ff64b87858e01642f
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-24 22:04:12 +0530

    BlackJack

--- Blackjack/main.py
+++ Blackjack/main.py
@@ -1,2 +1,18 @@
 import random
 
+def deal_card(): 
+    #Return a random card from the deck
+    cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+    card = random.choice(cards)
+    return card
+
+user_cards = []
+computer_cards = []
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+def calculated_cards():
+    return sum(user_cards), sum(computer_cards)
+
