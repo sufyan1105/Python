7814a233fc913ffde5050da561916d80d2c38e5d
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-07-21 16:14:17 +0530

    Coffee Machine Complete

--- Coffee Machine/main.py
+++ Coffee Machine/main.py
@@ -24,9 +24,65 @@ MENU = {
     }
 }
 
+profit = 0
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
+
+def is_resource_sufficient(order_ingredients):
+    for items in order_ingredients:
+        if order_ingredients[items] >= resources[items]:
+            print(f"Sorry, there is not enough {items}.")
+            return False
+    return True
+
+def process_coins():
+    print("Please insert coins.")
+    total = int(input("How many quarters? ")) * 0.25
+    total += int(input("How many dimes? ")) * 0.10
+    total += int(input("How many nickels? ")) * 0.05
+    total += int(input("How many pennies? ")) * 0.01
+    return total
+
+def transaction(money_received, drink_cost):
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit 
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry, that's not enough money. Money refunded.")
+        return False
+
+def make_coffee(drink_name, order_ingredients):
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}. Enjoy!")
+
+
+Machine_on = True
+
+while Machine_on:
+    choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if choice == "off":
+        Machine_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        if is_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            if transaction(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
+
+
+
+
+
 
