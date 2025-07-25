a73f89c65ba3432f7d2b4b01f7e962049b73582b
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-07-25 17:08:15 +0530

    Pretty table

--- Turtle Library/turtle_drawing.py
+++ Turtle Library/turtle_drawing.py
@@ -1,14 +1,19 @@
 # Importing Libraries
 # import turtle
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# my_screen = Screen()
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.forward(100)
+#
+# my_screen.exitonclick()
 
-timmy = Turtle()
-my_screen = Screen()
-timmy.shape("turtle")
-timmy.color("red")
-timmy.forward(100)
 
-my_screen.exitonclick()
+from prettytable import PrettyTable
 
+table = PrettyTable()
+print(table)
 
 
